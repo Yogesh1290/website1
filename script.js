51aac30af73aaa3cfb80bf011fa921f1707f7712
Yogesh1290 <sme50962@gmail.com>
2024-03-21 13:15:10 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scroll to section
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const section = document.querySelector(this.getAttribute('href'));
+      if (section) {
+        section.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+  
